467599343ff65d82863eb2b42c1afe3d2a24cac0
unknown <soumita018@gmail.com>
2019-08-24 23:00:40 +0530

    running

--- social/views.py
+++ social/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from social.models import Comment
 # Create your views here.
+
+def pehla(request):
+    p = Comment.objects.all()
+    return render(request,'index.html',{'p':p})
--- web/urls.py
+++ web/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from social.views import pehla
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',pehla,name="pehla"),   
 ]
