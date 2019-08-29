e241c37ee30f048141607c67bc920de95706e458
unknown <soumita018@gmail.com>
2019-08-29 20:55:06 +0530

    configured S3 for file storage

--- web/settings.py
+++ web/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'corsheaders',
     'graphene_django',
-
+    'storages',
     'social',
 ]
 
@@ -200,3 +200,12 @@ CORS_ALLOW_HEADERS = (
     'x-requested-with',
     'access-control-allow-origin'
 )
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
